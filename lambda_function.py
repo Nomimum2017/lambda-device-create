274ba443c855ca9cbe0c09185ba3f0e861d84a49
Alan Williams <alanwill81@gmail.com>
2016-04-02 00:00:35 -0700

    Testing for pre-existence of macaddr before adding a new one

--- lambda_function.py
+++ lambda_function.py
@@ -9,21 +9,30 @@ thingName = str(uuid.uuid4())
 def lambda_handler(event, context):
 
     iot = boto3.client('iot')
-    createdevice = iot.create_thing(thingName=thingName,
-                     attributePayload={
-                         'attributes': {
-                             'string': 'string'
-                         }
-                     }
-                )
-
     dynamodb = boto3.resource('dynamodb')
 
     table = dynamodb.Table('devices')
-
-    table.put_item(
-        Item={
-            'macaddr': event['body']['macaddr'],
-            'deviceid': createdevice['thingName'],
+    macaddr_check = table.get_item(
+        Key={
+            'macaddr': event['body']['macaddr']
         }
     )
+
+    if 'Item' in macaddr_check:
+        return "MAC address already exists. New device not created."
+    else:
+        createdevice = iot.create_thing(thingName=thingName,
+                                        attributePayload={
+                                            'attributes': {
+                                                'string': 'string'
+                                            }
+                                        }
+                                        )
+
+        table.put_item(
+            Item={
+                'macaddr': event['body']['macaddr'],
+                'deviceid': createdevice['thingName'],
+            }
+        )
+
