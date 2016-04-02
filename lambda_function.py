a96ae9da85bbc6da2fdffeba8eabcf0701eaae16
Alan Williams <alanwill81@gmail.com>
2016-04-02 02:11:35 -0700

    Create and attach cert to device

--- lambda_function.py
+++ lambda_function.py
@@ -5,34 +5,40 @@ import boto3
 import uuid
 
 thingName = str(uuid.uuid4())
+tevent = { "body" : { "macaddr": "abc1234f" }}
 
 def lambda_handler(event, context):
+
+    macAddr = tevent['body']['macaddr']
 
     iot = boto3.client('iot')
     dynamodb = boto3.resource('dynamodb')
 
     table = dynamodb.Table('devices')
-    macaddr_check = table.get_item(
+    macaddrCheck = table.get_item(
         Key={
-            'macaddr': event['body']['macaddr']
+            'macaddr': macAddr
         }
     )
 
-    if 'Item' in macaddr_check:
+    if 'Item' in macaddrCheck:
         return "MAC address already exists. New device not created."
     else:
-        createdevice = iot.create_thing(thingName=thingName,
+        print("macaddrCheck output: " + str(macaddrCheck.get()))
+        createDevice = iot.create_thing(thingName=thingName,
                                         attributePayload={
                                             'attributes': {
                                                 'string': 'string'
                                             }
                                         }
                                         )
+        print("createDevice output: " + str(createDevice.get()))
 
-        table.put_item(
+        insertDevice = table.put_item(
             Item={
-                'macaddr': event['body']['macaddr'],
-                'deviceid': createdevice['thingName'],
+                'macaddr': macAddr,
+                'deviceid': createDevice['thingName'],
             }
         )
+        print("insertDevice output: " + str(insertDevice.get()))
 
