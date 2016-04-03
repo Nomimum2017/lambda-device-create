a1e8862c3d1c4872739bd7d75d2716591b0a6ddd
Alan Williams <alanwill81@gmail.com>
2016-04-02 20:26:24 -0700

    Making all variables snd fields camelCase for consistency

--- lambda_function.py
+++ lambda_function.py
@@ -8,31 +8,31 @@ thingName = str(uuid.uuid4())
 
 def lambda_handler(event, context):
 
-    macAddr = event['body']['macaddr']
+    macAddr = event['body']['macAddr']
 
     iot = boto3.client('iot')
     dynamodb = boto3.resource('dynamodb')
     mapTable = dynamodb.Table('device-map')
     certTable = dynamodb.Table('device-certs')
 
-    macaddrCheck = table.get_item(
+    macAddrCheck = mapTable.get_item(
         Key={
-            'macaddr': macAddr
+            'macAddr': macAddr
         }
     )
 
-    if 'Item' in macaddrCheck:
-        print("macaddrCheck output: ", macaddrCheck['ResponseMetadata'])
+    if 'Item' in macAddrCheck:
+        print("macAddrCheck output: ", macAddrCheck['ResponseMetadata'])
         return { "message": "MAC address already exists. New device not created." }
     else:
         print("macAddrCheck output: ", macAddrCheck['ResponseMetadata'])
         createDevice = iot.create_thing(thingName=thingName)
         print("createDevice output: ", createDevice['ResponseMetadata'])
 
-        insertDevice = table.put_item(
+        insertDevice = mapTable.put_item(
             Item={
-                'macaddr': macAddr,
-                'deviceid': createDevice['thingName'],
+                'macAddr': macAddr,
+                'deviceId': createDevice['thingName'],
             }
         )
         print("insertDevice output: ", insertDevice['ResponseMetadata'])
@@ -50,8 +50,7 @@ def lambda_handler(event, context):
 
     insertCert = certTable.put_item(
         Item={
-            'macaddr': macAddr,
-            'deviceid': createDevice['thingName'],
+            'deviceId': createDevice['thingName'],
             'certificatePem': createCert['certificatePem'],
             'PublicKey': createCert['keyPair']['PublicKey'],
             'PrivateKey': createCert['keyPair']['PrivateKey'],
